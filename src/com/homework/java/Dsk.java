5457a5684a5eedf1f089420cd66a298c3fe29437
bjwns <1464311596@qq.com>
2018-05-08 23:12:10 +0800

    数组排序

--- src/com/homework/java/Dsk.java
+++ src/com/homework/java/Dsk.java
@@ -6,16 +6,39 @@ public class Dsk {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double monny =10000;
-		double lixi ;
-		for (int year =1;year<=5;year++) {
-			 lixi =monny*0.003;
-			 monny +=lixi;}
-		   System.out.println(monny);
+		int[] array = new int[7];
+		Scanner scanner = new Scanner(System.in);
+	    System.out.println("请输入 7 个整数: ");
+	    for (int i=0;i<7;i++) {
+	        array[i] = scanner.nextInt();
 		}
-			
-		
+	    
+	    array = bubbleSort(array);
+	    
+	    for (int i =0;i<array.length;i++) {
+	          System.out.print(array[i]+" ");
+	    }
 	}
+	    
+	private static int[] bubbleSort(int[] array) {
+		// TODO Auto-generated method stub
+     	  
+	    for (int j =0;j<array.length;j++) {
+	    	for(int k =0;k<array.length-j-1;k++) {
+	          if (array[k]>array[k+1]) {
+	        	  int tmp = array[k];
+	        	  array[k]= array[k+1];
+	        	  array[k+1] =tmp;}
+	        	  
+	    	}   
+	    } 	  
+		
+		
+		return array;
+	    	}
+	    }	
+	
+
 		
 
 
