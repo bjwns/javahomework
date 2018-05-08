6f9511bf03324790944571cf00b8995d8108072f
bjwns <1464311596@qq.com>
2018-05-08 20:33:31 +0800

    青蛙王子

---
+++ src/com/homework/java/Dsk.java
@@ -0,0 +1,25 @@
+package com.homework.java;
+
+import java.util.Scanner;
+
+public class Dsk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a =7;int i =0;
+		int day =0;
+		 while(true){
+			 day++;
+			i += 3;
+			if (i>=7) {
+				break;
+			}else {
+			i -= 2;}
+			
+		}System.out.println(day);
+	}
+		
+}
+
+	
