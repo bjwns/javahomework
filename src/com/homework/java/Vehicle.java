26b5d707d13562583f54f1064cd27d54698a22d5
bjwns <1464311596@qq.com>
2018-05-11 23:41:55 +0800

    作业

--- src/com/homework/java/Vehicle.java
+++ src/com/homework/java/Vehicle.java
@@ -1,9 +1,12 @@
 package com.homework.java;
 
 public class Vehicle {
-	  int speed ;
-      int size;
-	public void move() {
+	 int speed ;
+     int size;
+     public void show () {
+ 		System.out.println("速度"+speed);
+ 		System.out.println("体积"+size);}
+     public void move() {
 		System.out.println("车子可以移动");
 	}
 	public void setSpeed() {
@@ -14,6 +17,8 @@ public class Vehicle {
 	}
 	public void  speedDown() {
 		System.out.println("减速10时，车子速度为10");
-	}
-
+         
+         
+	}      
+	
 }
--- src/com/homework/java/VehicleTest.java
+++ src/com/homework/java/VehicleTest.java
@@ -7,14 +7,14 @@ public class VehicleTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Vehicle car =new Vehicle();
-		car.speed=100;
+		car.speed =100;
 		car.size =2500;
+		car.show();
 		car.move();
 		car.setSpeed();
 		car.speedUp();
 		car.speedDown();
-	
-	
+
 	}
 
 }
